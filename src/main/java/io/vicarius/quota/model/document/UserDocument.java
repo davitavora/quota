657ec1288a5592e2aaad274705b6a468f89fb130
Davi Herculino <davi.herculino.ext@nuclea.com.br>
2024-03-03 09:51:31 -0300

    fix: adds date format for elastic document

--- src/main/java/io/vicarius/quota/model/document/UserDocument.java
+++ src/main/java/io/vicarius/quota/model/document/UserDocument.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Setter
 @Getter
@@ -15,6 +18,8 @@ public class UserDocument {
     String id;
     private String firstName;
     private String lastName;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     private LocalDateTime lastLoginTimeUtc = LocalDateTime.now();
 
 }
